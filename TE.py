f1b5569142e418db73e43525cf7764459fa8008b
mathieu <mathieiu.vallee1998@gmail.com>
2023-02-27 10:36:11 +0100

    first commit

---
+++ TE.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numba import jit
+from itertools import combinations
+
+#@jit(cache=True)
+def det(M):
+    M = [row[:] for row in M] # make a copy to keep original M unmodified
+    N, sign, prev = len(M), 1, 1
+    for i in range(N-1):
+        if M[i][i] == 0: # swap with another row having nonzero i's elem
+            swapto = next( (j for j in range(i+1,N) if M[j][i] != 0), None )
+            if swapto is None:
+                return 0 # all M[*][i] are zero => zero determinant
+            M[i], M[swapto], sign = M[swapto], M[i], -sign
+        for j in range(i+1,N):
+            for k in range(i+1,N):
+                M[j][k] = ( M[j][k] * M[i][i] - M[j][i] * M[i][k] ) // prev
+        prev = M[i][i]
+    return sign * M[-1][-1]
+    
+def is_equimodular(M):
+	k,n = M.shape
+	first_not_null = 0
+	for iter_columns in combinations(range(n),k):
+		d = det(M[:,iter_columns])
+		if d!=0:
+			if not first_not_null:
+				first_not_null = abs(d)
+			if abs(d) != first_not_null:
+				return False
+	return(True)
+
+def is_TE(M):
+	k,n = M.shape
+	for i in range(1,n):
+		for iter_rows in combinations(range(k),i):
+			if not is_equimodular(M[iter_rows,:]):
+				return False
+	return True
---
+++ test_equim.sage
@@ -0,0 +1,37 @@
+import numpy as np
+import TE
+import tqdm
+
+def getPolyhedronFromGraph(g):
+	trivialConstraintsGe0 = np.array([np.zeros(g.order() +1) for i in range(0,g.order())])
+	c = 1
+	for j in trivialConstraintsGe0:
+		j[c]=1
+		c = c+1
+	cliqueOfg = np.array(list(g.all_cliques()))
+	cliqueConstraints = np.zeros((cliqueOfg.shape[0],g.order()+1))
+	c=0
+	for K in cliqueOfg:
+		cliqueConstraints[c][0] = 1
+		cliqueConstraints[c][K+1] = -1
+		c=c+1
+	constraintvectors = np.concatenate((trivialConstraintsGe0, cliqueConstraints))
+	return Polyhedron(ieqs = constraintvectors,base_ring=QQ)
+	
+f = open("perfect8.g6","r")
+
+G = graphs_list.from_whatever(f)
+
+N = len(G)
+for k in tqdm.tqdm(range(N)):
+	g = G[k]
+	P_g = getPolyhedronFromGraph(g)
+	if not P_g.is_compact():
+		continue
+	normal_fan = P_g.normal_fan()
+	for cone in normal_fan:
+		M = np.array(cone.rays())
+		if not TE.is_TE(M):
+			print(list(g.all_cliques()))
+			break
+	
