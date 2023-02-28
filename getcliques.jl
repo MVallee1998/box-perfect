584caff908c7bf8a43b500a95c3bb5d384c130bf
Alexandre Dupont-Bouillard <dupont-bouillard@lipn.univ-paris13.fr>
2023-02-28 10:50:22 +0100

    les cliques sont là mon fréro

---
+++ getcliques.jl
@@ -0,0 +1,63 @@
+#using LightGraphs
+using GraphIO
+using ParserCombinator
+using Test
+using Graphs
+
+
+
+function writeListEdges(inputstring, outputstring)
+
+	F = open(inputstring,"r")
+	
+	F1 = open(outputstring,"w")
+	
+	
+	
+	listG = loadgraphs(F,Graph6Format())
+	
+	for (key,g) in listG
+	
+	
+		all_cliques = maximal_cliques(g)
+		
+		print(F1, "[")
+		
+		for K in all_cliques
+			print(F1, "[")
+			for v in K
+			
+				print(F1,string(v-1) )
+				if(v != K[size(K)[1]])
+					print(F1,",")
+				
+				end
+			
+			
+			end
+			print(F1,"]")
+			if(K != all_cliques[size(all_cliques)[1]])
+				print(F1,",")
+			
+			end
+		
+		end
+		println(F1,"]")
+		
+		
+	
+	
+	end
+	
+	
+	close(F)
+	close(F1)
+
+
+end
+
+
+writeListEdges("graph8c.g6","all_cliques_8.txt")
+
+
+
